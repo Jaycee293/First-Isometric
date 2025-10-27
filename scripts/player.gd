420af88a7dcd8ce7c33b7984e0991e251e3cff7e
Davi Guilherme Tavares <daviguilherme1904@gmail.com>
2025-10-27 02:02:44 -0300

    hit nas setinhas

--- scripts/player.gd
+++ scripts/player.gd
@@ -16,8 +16,16 @@ func _physics_process(delta: float) -> void:
 		move_and_slide()
 		
 		#attack
-		if Input.is_action_just_pressed("attack"):
+		if Input.is_action_just_pressed("attack_down"):
 			_do_attack()
+		if Input.is_action_just_pressed("attack_up"):
+			_do_attack()
+		if Input.is_action_just_pressed("attack_left"):
+			_do_attack()
+		if Input.is_action_just_pressed("attack_right"):
+			_do_attack()
+
+			
 			
 		#play animations for each side that it moves
 		if velocity.length() > 0.0:
@@ -76,22 +84,33 @@ func die():
 
 func _do_attack(): 
 	# Hide the regular sprite and show the attack sprite
+	
 	animated_sprite_2d.hide()
 	attack.show()
-	match last_facing:
-		"down":
-			animation_player.play("front_attack")
-		"up":
-			animation_player.play("back_attack")
-		"left":
-			animation_player.play("left_attack")
-		"right":
-			animation_player.play("right_attack")
-		
+	
+	if Input.is_action_just_pressed("attack_down"):
+				animation_player.play("front_attack")
+				last_facing = "down"
+				
+	if Input.is_action_just_pressed("attack_up"):
+				animation_player.play("back_attack")
+				last_facing = "up"
+	
+	if Input.is_action_just_pressed("attack_left"):
+				animation_player.play("left_attack")
+				last_facing = "left"
+				
+	if Input.is_action_just_pressed("attack_right"):
+				animation_player.play("right_attack")
+				last_facing = "right"
+
+				
 	await animation_player.animation_finished
 	attack.hide()
 	animated_sprite_2d.show()
+	
 
 
 func _on_hp_zero() -> void:
 	%"Game over".visible = true
+	
